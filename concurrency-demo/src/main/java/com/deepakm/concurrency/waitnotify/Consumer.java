7e3757c0b62fc32fb5a0b91697893690468bd444
deepak marathe <Deepak_Marathe@intuit.com>
2015-05-20 02:55:24 +0530

    sequential producer consumer using wait and notify synchronization primitives

---
+++ concurrency-demo/src/main/java/com/deepakm/concurrency/waitnotify/Consumer.java
@@ -0,0 +1,19 @@
+package com.deepakm.concurrency.waitnotify;
+
+
+public class Consumer implements Runnable {
+    private final Counter counter;
+
+    public Consumer(Counter counter) {
+        this.counter = counter;
+    }
+
+    public void run() {
+        while (!counter.isComplete()) {
+            counter.print();
+        }
+        counter.setCompleted();
+        System.out.println("consumer done!");
+    }
+
+}
