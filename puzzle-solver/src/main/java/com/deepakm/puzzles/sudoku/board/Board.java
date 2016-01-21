5fe72fb205f6d38d908171333a1d2fe88090f67f
deepak marathe <Deepak_Marathe@intuit.com>
2016-01-21 17:51:36 +0530

    sudoku solver using backtracking algorithm

---
+++ puzzle-solver/src/main/java/com/deepakm/puzzles/sudoku/board/Board.java
@@ -0,0 +1,20 @@
+package com.deepakm.puzzles.sudoku.board;
+
+/**
+ * Created by dmarathe on 1/21/16.
+ */
+public interface Board {
+
+    public void initialise(int[][] puzzle);
+
+    public int getRows();
+    public int getColumns();
+    public int getSubGridSize();
+
+    public int get(int row, int column);
+    public void set(int row, int column, int number);
+    public void unset(int row, int column);
+    public boolean isSet(int row, int column);
+
+    public void printBoard();
+}
