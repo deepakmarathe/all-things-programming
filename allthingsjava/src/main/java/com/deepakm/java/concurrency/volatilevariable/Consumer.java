ac895d293e88383c8f4d7344521121cf569fdca9
deepak marathe <Deepak_Marathe@intuit.com>
2016-10-03 16:27:40 +0530

    adding concurrency examples - volatile

---
+++ allthingsjava/src/main/java/com/deepakm/java/concurrency/volatilevariable/Consumer.java
@@ -0,0 +1,25 @@
+package com.deepakm.java.concurrency.volatilevariable;
+
+/**
+ * Created by dmarathe on 10/3/16.
+ */
+public class Consumer implements Runnable {
+    private final Exchanger exchanger;
+
+    public Consumer(final Exchanger sharedResource){
+        this.exchanger = sharedResource;
+    }
+
+    @Override
+    public void run() {
+        while(true){
+            Object o = exchanger.take();
+            System.out.println("consumed object o : " + o);
+            try{
+                Thread.sleep(2000);
+            }catch(InterruptedException e){
+                e.printStackTrace();
+            }
+        }
+    }
+}
