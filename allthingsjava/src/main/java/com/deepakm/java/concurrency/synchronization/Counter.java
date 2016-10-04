e85f84da759b60801bf555e674a51078009934b4
deepak marathe <Deepak_Marathe@intuit.com>
2016-10-05 02:36:39 +0530

    adding synchronization examples

---
+++ allthingsjava/src/main/java/com/deepakm/java/concurrency/synchronization/Counter.java
@@ -0,0 +1,13 @@
+package com.deepakm.java.concurrency.synchronization;
+
+/**
+ * Created by dmarathe on 10/5/16.
+ */
+public class Counter {
+    long count = 0;
+
+    public synchronized void add(long value) {
+        this.count += value;
+        System.out.println("Thread name : " + Thread.currentThread().getName() + ", added  : " + value + ", total : " + count);
+    }
+}
