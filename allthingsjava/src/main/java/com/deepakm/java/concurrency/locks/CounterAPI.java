f1293698c4fe5d5a221c629d4fc3dc1728ae0c04
deepak marathe <Deepak_Marathe@intuit.com>
2016-10-19 13:25:12 +0530

    adding blocking queue implementation for flip counter implementation

---
+++ allthingsjava/src/main/java/com/deepakm/java/concurrency/locks/CounterAPI.java
@@ -0,0 +1,10 @@
+package com.deepakm.java.concurrency.locks;
+
+/**
+ * Created by dmarathe on 10/19/16.
+ */
+public interface CounterAPI {
+    public void increment();
+
+    public void print();
+}
