24621efdfd97475a97e152e640d19bd68f78a6fa
deepak marathe <Deepak_Marathe@intuit.com>
2016-10-21 16:59:10 +0530

    adding countdown latch and cyclic barrier example usages

---
+++ allthingsjava/src/main/java/com/deepakm/java/concurrency/countdownlatch/CountDownLatchExample.java
@@ -0,0 +1,30 @@
+package com.deepakm.java.concurrency.countdownlatch;
+
+import java.util.concurrent.CountDownLatch;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ * Created by dmarathe on 10/21/16.
+ */
+public class CountDownLatchExample {
+    private static final Logger logger = Logger.getLogger(CountDownLatchExample.class.getName());
+
+    public static void main(String[] args) {
+        final CountDownLatch countDownLatch = new CountDownLatch(3);
+        Thread cacheService = new Thread(new Service("cache service", 1000, countDownLatch));
+        Thread alertService = new Thread(new Service("alert service", 1000, countDownLatch));
+        Thread validationService = new Thread(new Service("validation service", 1000, countDownLatch));
+
+        cacheService.start();
+        alertService.start();
+        validationService.start();
+
+        try {
+            countDownLatch.await();
+        } catch (InterruptedException e) {
+            logger.log(Level.SEVERE, e.getMessage(), e);
+        }
+        System.out.println("Main execution has started.");
+    }
+}
