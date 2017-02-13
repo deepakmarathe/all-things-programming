aa2187f179159dd1d11cfe9de09d19d9712ce75d
Deepak Marathe <deepak.marathe@go-jek.com>
2017-02-13 08:58:23 +0530

    concurrency code commit

---
+++ concurrency-demo/src/main/java/volatilevariable/Producer.java
@@ -0,0 +1,26 @@
+package volatilevariable;
+
+/**
+ * Created by dmarathe on 10/3/16.
+ */
+public class Producer implements Runnable {
+    private final Exchanger exchanger;
+
+    public Producer(Exchanger sharedResource){
+        this.exchanger = sharedResource;
+    }
+
+    @Override
+    public void run() {
+        while(true){
+            Object o = new Object();
+            exchanger.put(o);
+            System.out.println("Producing new object : " + o);
+            try {
+                Thread.sleep(2000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
---
+++ concurrency-demo/src/main/java/waitnotify/Counter.java
@@ -0,0 +1,51 @@
+package waitnotify;
+
+public class Counter {
+    private volatile Long counter;
+    private Long LIMIT;
+
+    public Counter() {
+        this.counter = 0L;
+        withLimit(0L);
+    }
+
+    public Counter withLimit(Long limit) {
+        this.LIMIT = limit;
+        return this;
+    }
+
+    public synchronized void increment() {
+        counter = counter + 1;
+        if (counter <= LIMIT) {
+            System.out.println("Incremented to : " + counter);
+            this.notify();
+            try {
+                this.wait();
+            } catch (InterruptedException e) {
+                //retry logic
+                System.out.println("Retrying to wait");
+            }
+        }
+    }
+
+    public synchronized void print() {
+        System.out.println("printing : " + counter);
+        if (counter <= LIMIT) {
+            this.notify();
+            try {
+                this.wait();
+            } catch (InterruptedException e) {
+                System.out.println("retrying logic");
+            }
+        }
+    }
+
+    public synchronized boolean isComplete() {
+        return counter > LIMIT;
+    }
+
+    public synchronized void setCompleted() {
+        counter = LIMIT + 1;
+        notify();
+    }
+}
